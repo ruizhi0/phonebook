2cf02fbfc19403939e68f7b5558186e6c3bc152b
Rui Zhi Liew <ruizhi@outlook.my>
2023-06-09 00:40:46 +0800

    feat: get single person by id

--- index.js
+++ index.js
@@ -34,6 +34,17 @@ app.get("/api/info", (req, res) => {
   res.send(`<p>${phonebookInfo}<p><p>${requestDateTime}<p>`);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
