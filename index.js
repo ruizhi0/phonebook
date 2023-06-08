ec75615eae69caf1e5b927d47e1a8663cd538543
Rui Zhi Liew <ruizhi@outlook.my>
2023-06-09 00:35:20 +0800

    feat: get phonebook total entries

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/info", (req, res) => {
+  const phonebookInfo = `Phonebook has info for ${persons.length} people`;
+  const requestDateTime = new Date().toString();
+  res.send(`<p>${phonebookInfo}<p><p>${requestDateTime}<p>`);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
