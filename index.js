312d0a07dd98418b86895eed893f7ac8904e175e
Rui Zhi Liew <ruizhi@outlook.my>
2023-06-12 22:25:28 +0800

    feat: log http request body

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-app.use(morgan("tiny"));
+morgan.token("body", (req, res) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 app.use(express.json());
 
 let persons = [
